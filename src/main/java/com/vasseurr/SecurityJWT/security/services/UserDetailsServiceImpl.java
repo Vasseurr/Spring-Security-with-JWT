d7685bf8d5ee2b8ec87fa03d914d58a42147a39b
Okan <67058617+Vasseurr@users.noreply.github.com>
2021-03-27 20:26:59 +0300

    v1.1
    -add jwt filter

---
+++ src/main/java/com/vasseurr/SecurityJWT/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.vasseurr.SecurityJWT.security.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.vasseurr.SecurityJWT.entities.User;
+import com.vasseurr.SecurityJWT.repositories.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+		
+		//build a UserDetails object using static build() method
+		return UserDetailsImpl.build(user);
+	}
+
+}
